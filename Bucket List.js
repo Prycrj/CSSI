3b52eb70fd7c753dd451cac58a29fdd21841790b
Rayona <Rayona1@gmail.com>
2017-08-01 14:54:39 -0400

    New Bucket Update

--- Bucket List.js
+++ Bucket List.js
@@ -1,22 +1,4 @@
-<script>
 
-    <ol>
-      <li id="todo1">Graduate with honors
-      <button onclick="Task1Done('todo1')">PUSH ME</button>
-      </li>
-      <li id="todo2">Travel the World
-      <button onclick="Task1Done('todo2')">PUSH ME</button>
-      </li>
-      <li id="todo3"> Visit Google NYC
-      <button onclick="Task1Done('todo3')">PUSH ME</button>
-      </li>
-      <li id="todo4"> Sky Dive
-      <button onclick="Task1Done('todo4')">PUSH ME</button>
-      </li>
-      <li id="todo5"> Sling-Shot Ride
-      <button onclick="Task1Done('todo5')">PUSH ME</button>
-      </li>
-     </ol>
 
 function Task1Done(Boom)
 {
@@ -29,4 +11,9 @@ else
  document.getElementById(Boom).style.textDecoration='line-through';
 
 }
-</script>
+function clearAll()
+{
+  names=document.getElementsByClassName('taskdone')
+for (i=0; i<names.length; i++)
+names[i].getElementById('taskdone').style.textDecoration='none';
+}
